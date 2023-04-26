3fee048006a2328408760a60410da08fad0dc6d1
Crowdbotics <team@crowdbotics.com>
2023-04-26 01:53:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-credit-40224",
+  url: "https://billowing-credit-40224.botics.co",
+  api: "https://billowing-credit-40224.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
